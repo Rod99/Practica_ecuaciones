9ae220850f269a7c56ebca0cc721948e944840ef
Rod99 <36975715+Rod99@users.noreply.github.com>
2018-03-06 08:51:04 -0600

    Actualización TADPilaDinSW.c
    Se agregaron las funciones Destroy, Empty y Pop

--- TADPilaDinSW.c
+++ TADPilaDinSW.c
@@ -8,7 +8,7 @@
 
 	DESCRIPCION DE PILA:
 		Estructura de datos en la que se cumple: 
-			los elementos se a�aden y se remueven por un solo extremo			 
+			los elementos se añaden y se remueven por un solo extremo			 
 */
 //LIBRERIAS
 #include<stdio.h>
@@ -82,36 +82,91 @@ elemento Pop(pila *s)
 	return r;
 }
 /*
-	***void Push(pila *s, elemento e)***
+	***boolean Empty(pila s*)***
 
-		Descripcion: 
-		Recibe:
-		Devuelve:
+		Descripcion:Pregunta si la pila esta vacia 
+		Recibe: int *s ("s" a operar)
+		Devuelve:Si la pila esta vacia o tiene algo adentro (TRUE OR FALSE)
 		Observaciones:	
 */
+boolean Empty(pila *s)
+{
+	boolean r;
+	if(s->tope==NULL)
+	{
+		r=TRUE;
+	}	
+	else
+	{
+		r=FALSE;
+	}
+	return r;	
+}
 
 
 /*
-	***void Push(pila *s, elemento e)***
+	***elemento top pila (pila *s)***
 
-		Descripcion: 
-		Recibe:
-		Devuelve:
-		Observaciones:	
+		Descripcion:tope (muestra el valor que esta hasta arriba de la pila sin extraerlo)
+		Recibe: int *s ("s" a operar)
+		Devuelve:elemento (ultimo elemento en entrar a la pila sin extraer dicho elemento)
+		Observaciones:No se valida si la pila esta vacia antes de consultar al elemento del tope (causa error si esta esta vacia).
 */
+elemento Top(pila *s)
+{
+	//Si el tope es NULL, se intenta Top de pla vacia 
+	if(s->tope==NULL)
+	{
+		printf("\nERROR-e=Top(&s): Subdesbordamiento de la pila, no hay elementos en la pila");
+		exit(1);
+	}
+	
+	return s->tope->e;
+}
+
 /*
-	***void Push(pila *s, elemento e)***
+	***int size (pila *s)***
 
-		Descripcion: 
-		Recibe:
-		Devuelve:
-		Observaciones:	
+		Descripcion:tamaño de la pila (Numero de elementos que contiene la pila)
+		Recibe:int  *s
+		Devuelve:int (tamaño de la pila 0-> vacia , y el numero de elemento que la contiene 1->1, 2->2)
+		Observaciones:s tiene la referencia a la pila
 */
+int Size(pila *s)
+{
+	nodo *aux;
+	int tam_pila=0;
+	aux=s->tope;
+	if(aux!=NULL)
+	{
+		tam_pila++;
+		while(aux->abajo!=NULL)
+		{
+			tam_pila++;
+			aux=aux->abajo;
+		}
+	}	
+	return tam_pila;
+}
 /*
-	***void Push(pila *s, elemento e)***
+	***void destroy (pila*s)***
 
-		Descripcion: 
-		Recibe:
-		Devuelve:
-		Observaciones:	
+		Descripcion:Elimina a la pila (borra todos los elementos y la memoria de la pila)
+		Recibe:int *s
+		Devuelve:---
+		Observaciones:	---
 */
+void Destroy(pila *s)
+{
+	nodo *aux;
+	if(s->tope!=NULL)
+	{
+		while(s->tope!=NULL)
+		{				
+			aux=s->tope->abajo;
+			free(s->tope);
+			s->tope=aux;
+		}		
+	}
+	return;
+}
